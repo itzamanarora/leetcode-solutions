8e32eb2d6d1f54929140b5146c0820d5fbf89c61
Aman Arora <43453929+itzamanarora@users.noreply.github.com>
2026-08-27 17:41:00 +0530

    Time: 0 ms (100%), Space: 48 MB (87.76%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,44 +1,32 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = findFirst(nums, target);
-        int last = findLast(nums, target);
+        int first = search(nums, target, true);
+        int last = search(nums, target, false);
         return new int[] {first, last};
     }
 
-    private static int findFirst(int[]nums, int target) {
+    private static int search(int[] nums, int target, boolean startIndex) {
         int start = 0;
         int end = nums.length-1;
-        int first = -1;
+        int middle;
+        int ans = -1;
 
         while(start <= end) {
-            int middle = start + (end - start) / 2;
+            middle = start + (end - start) / 2;
 
-            if(nums[middle] == target) {
-                first = middle;
-                end = middle-1;
+            if(nums[middle] < target) start = middle + 1;
+            else if(nums[middle] > target) end = middle -1;
+            else {
+                if(startIndex) {
+                    ans = middle;
+                    end = middle -1;
+                } else {
+                    ans = middle;
+                    start = middle + 1;
+                }
             }
-            else if(nums[middle] < target) start = middle + 1;
-            else end = middle - 1;
         }
-        return first;
+        return ans;
     }
 
-    private static int findLast(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length-1;
-        int last = -1;
-
-        while(start <= end) {
-            int middle = start + (end - start) / 2;
-
-            if(nums[middle] == target) {
-                last = middle;
-                start = middle + 1;
-            }
-            else if(nums[middle] < target) start = middle + 1;
-            else end = middle - 1;
-        }
-
-        return last;
-    }
 }
